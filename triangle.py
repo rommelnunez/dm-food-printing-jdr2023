bc91b9916f061b5910ed7cac3617a68e8681839e
Rommel Núñez <rn2500@columbia.edu>
2023-04-13 15:48:43 -0400

    Finished code : )

--- triangle.py
+++ triangle.py
@@ -3,6 +3,9 @@
 # @Description: g-code generator for food printer that prints a triangle
 # @Author: Philippe Wyder (PMW2125)
 #
+
+import math
+
 FEEDRATE = 200
 EXTRUSION = 0.5
 import argparse
@@ -16,9 +19,44 @@ def main(side_length):
     # Controlled Movement to X,Y,Z position while Extruding 1-unit at Feedrate (speed) 50[cm/min]
     # NOTE: Z-VALUE can be omited and insteda set Z-height for single layer on printer
     lines.append(f"G1 E0.25" + eol) # Extrude a small amount (prime nozzle)
-    lines.append(f"G1 X0                Y{side_length}  Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
-    lines.append(f"G1 X{side_length}    Y{side_length}  Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
-    lines.append(f"G1 X0                Y0              Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
+
+    #TEMPLATE FOR DRAWING TRIANGLE
+    # lines.append(f"G1 X0                Y{side_length}  Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
+    # lines.append(f"G1 X{side_length}    Y{side_length}  Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
+    # lines.append(f"G1 X0                Y0              Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
+    # lines.append(f"G1 Z1.0" + eol)
+
+    #PRINTING SECTION: this part of the code calculatest the distance from the center of triangles
+    #   to the next vertex. It uses a cos/sin function to turn the next coordinate.
+
+    # Parameters
+    center_x = 0   # x-coordinate of the center of the triangles
+    center_y = 0   # y-coordinate of the center of the triangles
+    radius = 16   # distance from the center of the triangle to its corners
+    delta = math.pi/15  # angle by which to offset the next triangle (12 degrees)
+    scale_factor = 0.9  # factor by which to reduce the radius of each triangle
+    theta = 0
+    z = 0 #tweaks how far the previous triangle is from the next (z-movement)
+
+    for i in range(10):  # draw 10 triangles
+        # Calculate coordinates of the triangle vertices
+        x1 = center_x + radius * math.cos(theta)
+        y1 = center_y + radius * math.sin(theta)
+        x2 = center_x + radius * math.cos(theta + 2*math.pi/3)
+        y2 = center_y + radius * math.sin(theta + 2*math.pi/3)
+        x3 = center_x + radius * math.cos(theta + 4*math.pi/3)
+        y3 = center_y + radius * math.sin(theta + 4*math.pi/3)
+
+        lines.append(f"G1 X{x1}                Y{y1}  Z{z} E{EXTRUSION} F{FEEDRATE}" + eol)
+        lines.append(f"G1 X{x2}                Y{y2}  Z{z} E{EXTRUSION} F{FEEDRATE}" + eol)
+        lines.append(f"G1 X{x3}                Y{y3}  Z{z} E{EXTRUSION} F{FEEDRATE}" + eol)
+        
+        # Update parameters for the next triangle
+        radius *= scale_factor
+        theta += delta
+        z+=1
+
+    lines.append(f"G1 Z1.0" + eol)
     lines.append("G1 Z5 E-2 F200" + eol) # move up & retract plunger a bit
     lines.append("G1 X25 F400 " + eol) # move away from the print
     lines.append("G28") # Home printer
