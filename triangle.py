b492ca4ba98b4bcd14c6f96027091022e9296c33
David Cendejas <daveeed@Davids-MacBook-Pro-45.local>
2023-04-29 13:57:23 -0400

    triangle.py

--- triangle.py
+++ triangle.py
@@ -18,46 +18,48 @@ def main(side_length):
     lines.append("G90"+ eol) # Send absolute position commands
     # Controlled Movement to X,Y,Z position while Extruding 1-unit at Feedrate (speed) 50[cm/min]
     # NOTE: Z-VALUE can be omited and insteda set Z-height for single layer on printer
-    lines.append(f"G1 E0.25" + eol) # Extrude a small amount (prime nozzle)
-
-    #TEMPLATE FOR DRAWING TRIANGLE
-    # lines.append(f"G1 X0                Y{side_length}  Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
-    # lines.append(f"G1 X{side_length}    Y{side_length}  Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
-    # lines.append(f"G1 X0                Y0              Z0 E{EXTRUSION} F{FEEDRATE}" + eol)
-    # lines.append(f"G1 Z1.0" + eol)
-
-    #PRINTING SECTION: this part of the code calculatest the distance from the center of triangles
-    #   to the next vertex. It uses a cos/sin function to turn the next coordinate.
+    
 
     # Parameters
     center_x = 0   # x-coordinate of the center of the triangles
     center_y = 0   # y-coordinate of the center of the triangles
     radius = 16   # distance from the center of the triangle to its corners
-    delta = math.pi/15  # angle by which to offset the next triangle (12 degrees)
-    scale_factor = 0.9  # factor by which to reduce the radius of each triangle
+    delta = math.pi/40  # angle by which to offset the next triangle (12 degrees)
+    scale_factor = 25  # factor by which to reduce the radius of each triangle
     theta = 0
     z = 0 #tweaks how far the previous triangle is from the next (z-movement)
-
-    for i in range(10):  # draw 10 triangles
-        # Calculate coordinates of the triangle vertices
-        x1 = center_x + radius * math.cos(theta)
-        y1 = center_y + radius * math.sin(theta)
-        x2 = center_x + radius * math.cos(theta + 2*math.pi/3)
-        y2 = center_y + radius * math.sin(theta + 2*math.pi/3)
-        x3 = center_x + radius * math.cos(theta + 4*math.pi/3)
-        y3 = center_y + radius * math.sin(theta + 4*math.pi/3)
-
-        lines.append(f"G1 X{x1}                Y{y1}  Z{z} E{EXTRUSION} F{FEEDRATE}" + eol)
-        lines.append(f"G1 X{x2}                Y{y2}  Z{z} E{EXTRUSION} F{FEEDRATE}" + eol)
-        lines.append(f"G1 X{x3}                Y{y3}  Z{z} E{EXTRUSION} F{FEEDRATE}" + eol)
+    for layer in range(0, 5, 1):
         
-        # Update parameters for the next triangle
-        radius *= scale_factor
-        theta += delta
-        z+=2
+        lines.append(f"G1 E0.25" + eol) # Extrude a small amount (prime nozzle)
+        for i in range(0,108,1):  # draw 10 triangles
+            j = i * .3
+            r = scale_factor * math.cos(-.4 * j)
+            # Calculate coordinates of the triangle vertices
+            x = r * math.cos(j)
+            y =r * math.sin(j)
 
-    lines.append(f"G1 Z1.0" + eol)
-    lines.append("G1 Z5 E-2 F200" + eol) # move up & retract plunger a bit
+            lines.append(f"G1 X{x}                Y{y}  Z{z} E{EXTRUSION} F{FEEDRATE}" + eol)
+            
+            # Update parameters for the next triangle
+            radius *= scale_factor
+            theta += delta
+            
+        if layer % 2 == 0:
+        # Select tool number 2 and apply an offset of 18mm to the left
+            lines.append(f"G1 Z{z} E-.25 F200" + eol) # move up & retract plunger a bit
+            lines.append("T1 M6" + eol)
+            
+            lines.append(f"G1 X-18 E0 F200" + eol)
+            
+        else:
+            lines.append(f"G1 Z{z} E-.25 F200" + eol) # move up & retract plunger a bit
+            lines.append("T0 M6" + eol)
+            lines.append(f"G1 X18 E0 F200" + eol)
+        
+        z = layer
+        
+
+    lines.append("G1 Z{z} E-2 F200" + eol) # move up & retract plunger a bit
     lines.append("G1 X25 F400 " + eol) # move away from the print
     lines.append("G28") # Home printer
     # write lines to file
